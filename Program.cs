33a7a1efcf4298ad970f926cf015b89a17db17b1
Phan Huy <phanhuy0108@gmail.com>
2019-10-01 10:03:46 +0700

    Create Program.cs
    ex

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+        public static bool IsOdd(int n)
+        {
+            if (n % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            return true;
+        }
+
+        public static int Square(int n)
+        {
+            return n * n;
+        }
+
+        public static int Cube(int n)
+        {
+            return n * n * n;
+        }
+
+        public static int Pow(int x, int y)
+        {
+            int rs = 1;
+            for (int i = 1; i <= y; i++)
+                rs = rs * x;
+            return rs;
+        }
+    }
+}
+
+    
+
